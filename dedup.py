643ca6868d59bd6f902f7142938be5b4f6b2d0b8
Pascal Schmid <175302205+passchm@users.noreply.github.com>
2024-09-10 00:00:56 +0200

    Implement basic scanning

--- dedup.py
+++ dedup.py
@@ -15,9 +15,129 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import datetime
+import hashlib
+import sqlite3
+import sys
+from dataclasses import dataclass
+from enum import Enum
+from pathlib import Path
+
+SQL_INIT = """
+CREATE TABLE IF NOT EXISTS files (
+    kind INTEGER,
+    path TEXT,
+    size INTEGER,
+    timestamp INTEGER,
+    checksum TEXT
+);
+"""
+
+
+class EntryKind(Enum):
+    FILE = 1
+    DIRECTORY = 2
+    SYMLINK = 3
+
+
+@dataclass(frozen=True)
+class Entry:
+    kind: EntryKind
+    path: Path
+    size: int
+    timestamp: datetime.datetime
+    checksum: str
+
+
+class UnknownEntryKindError(NotImplementedError):
+    pass
+
+
+def checksum_file(file_path: Path) -> str:
+    with open(file_path, "rb") as f:
+        return hashlib.file_digest(f, "sha256").hexdigest()
+
+
+def scan_dir(dir_path: Path):
+    items = []
+    for path in sorted(dir_path.iterdir()):
+        if path.is_symlink():
+            items.append(
+                Entry(
+                    EntryKind.SYMLINK,
+                    path,
+                    path.lstat().st_size,
+                    datetime.datetime.fromtimestamp(
+                        path.lstat().st_mtime,
+                        tz=datetime.timezone.utc,
+                    ),
+                    None,
+                )
+            )
+        elif path.is_dir():
+            assert not path.is_symlink()
+            items.append(
+                Entry(
+                    EntryKind.DIRECTORY,
+                    path,
+                    path.lstat().st_size,
+                    datetime.datetime.fromtimestamp(
+                        path.lstat().st_mtime,
+                        tz=datetime.timezone.utc,
+                    ),
+                    None,
+                )
+            )
+            items.extend(scan_dir(path))
+        elif path.is_file():
+            assert not path.is_symlink()
+            items.append(
+                Entry(
+                    EntryKind.FILE,
+                    path,
+                    path.lstat().st_size,
+                    datetime.datetime.fromtimestamp(
+                        path.lstat().st_mtime,
+                        tz=datetime.timezone.utc,
+                    ),
+                    checksum_file(path),
+                )
+            )
+        else:
+            raise UnknownEntryKindError("unknown kind of path " + repr(path))
+    return items
+
 
 def main():
-    pass
+    target_dir = Path(sys.argv[1])
+    print(target_dir)
+
+    conn = sqlite3.connect("dedup.sqlite3")
+
+    conn.execute(SQL_INIT)
+    conn.commit()
+
+    entries = scan_dir(target_dir)
+    print(len(entries))
+
+    mapped_entries = map(
+        lambda e: (
+            e.kind.value,
+            str(e.path),
+            e.size,
+            e.timestamp.timestamp(),
+            e.checksum,
+        ),
+        entries,
+    )
+    conn.executemany(
+        "INSERT INTO files (kind, path, size, timestamp, checksum)"
+        + "VALUES (?, ?, ?, ?, ?)",
+        mapped_entries,
+    )
+    conn.commit()
+
+    conn.close()
 
 
 if __name__ == "__main__":
