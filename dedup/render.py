9ea0b35b0df7de6aeeea2e81e72bd5f18be93c4b
Pascal Schmid <175302205+passchm@users.noreply.github.com>
2024-09-10 17:19:39 +0200

    Add basic plain text rendering

--- dedup/render.py
+++ dedup/render.py
@@ -15,11 +15,70 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import argparse
+import datetime
+import sqlite3
+from collections import defaultdict
+from pathlib import Path
+
+from .scan import Entry, EntryKind
 from .xhtml5builder import XHT
 
 
+def convert_row_to_entry(row: tuple) -> Entry:
+    return Entry(
+        EntryKind(row[0]),
+        Path(row[1]),
+        row[2],
+        datetime.datetime.fromtimestamp(row[3], tz=datetime.timezone.utc),
+        row[4],
+    )
+
+
+def build_tree(paths):
+    tree = lambda: defaultdict(tree)
+    root = tree()
+
+    for path in paths:
+        current_level = root
+        for part in path.parts:
+            current_level = current_level[part]
+
+    return root
+
+
+def print_tree(d, level=0):
+    for key, value in d.items():
+        print("    " * level + str(key))
+        if isinstance(value, dict):
+            print_tree(value, level=level + 1)
+
+
 def main():
-    print(XHT)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--database", default="./dedup.sqlite3", help="the database file"
+    )
+    args = parser.parse_args()
+
+    conn = sqlite3.connect(args.database)
+
+    raw_paths = frozenset(
+        sorted(
+            map(
+                lambda row: Path(row[0]),
+                conn.execute("SELECT DISTINCT path FROM files ORDER BY path"),
+            )
+        )
+    )
+
+    for row in conn.execute("SELECT * FROM files ORDER BY path"):
+        entry = convert_row_to_entry(row)
+
+    conn.close()
+
+    bare_tree = build_tree(raw_paths)
+    print_tree(bare_tree)
 
 
 if __name__ == "__main__":
